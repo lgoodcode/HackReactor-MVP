4d79fbb330c89430adfdb903b358666e3d94fd60
Lawrence Good <lawrence.good55@gmail.com>
2022-11-17 14:04:04 -0800

    [Client] Refactor server.js

--- client/server.js
+++ client/server.js
@@ -1,17 +1,18 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import express from 'express'
 import { join, dirname } from 'path'
 import { fileURLToPath } from 'url'
 import compression from 'compression'
 
 const app = express()
+// __dirname alternative since process.env is not available when using "module" type
+// https://stackoverflow.com/questions/46745014/alternative-for-dirname-in-node-js-when-using-es6-modules
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const PORT = process.env.PORT || 3000
 
 app.use(compression())
 app.use(express.static(join(__dirname)))
 
-app.get('*', (req, res) => {
+app.get('*', (_, res) => {
   res.sendFile(join(__dirname, 'dist', 'index.html'))
 })
 
