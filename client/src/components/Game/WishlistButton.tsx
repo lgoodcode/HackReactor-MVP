003d8684677088afb2e9869e542f9ec8a8924675
Lawrence Good <lawrence.good55@gmail.com>
2022-11-22 16:23:27 -0800

    [Client] Changed wishlist active bg color

--- client/src/components/Game/WishlistButton.tsx
+++ client/src/components/Game/WishlistButton.tsx
@@ -8,7 +8,7 @@ export default function WishlistButton({ remove }: WishlistButtonProps) {
   return (
     <div
       onClick={remove}
-      className="wished game-card-btn centered gap-2 !bg-sky-500 hover:!bg-sky-400"
+      className="wished game-card-btn centered gap-2 !bg-lavender-500 hover:!bg-lavender-400"
     >
       <GiftIcon width={16} height={16} className="fill-white pointer-events-none" />
     </div>
